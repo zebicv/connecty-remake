1da26707bffa92e6e31a6ee1539bd6fb60271a2b
Boris Antonijev <81489899+fearwarden@users.noreply.github.com>
2024-04-01 11:43:47 +0200

    [backend] feat: rest constants

---
+++ api/src/constants/HTTPResponse.ts
@@ -0,0 +1,13 @@
+export class HTTPResponses {
+  static readonly OK = "Ok.";
+  static readonly BAD_REQUEST = "Bad request.";
+  static readonly INVALID_DATA = "Invalid data sent.";
+  static readonly USER_EXIST = "User already exists.";
+  static readonly USER_DOES_NOT_EXIST = "User doesn't exist.";
+  static readonly INTERNAL_SERVER_ERROR = "Internal server error.";
+  static readonly INVALID_USER = "Invalid user credentials.";
+  static readonly UNAUTHORIZED = "Unauthorized.";
+  static readonly INCORRECT_PASSWORD = "Incorrect password.";
+  static readonly IMAGE_ALREADY_EXIST = "Image already exist in that folder.";
+  static readonly POST_NOT_FOUND = "Post not found.";
+}
---
+++ api/src/constants/HTTPStatusCode.ts
@@ -0,0 +1,11 @@
+export class HTTPStatusCode {
+  static readonly INTERNAL_SERVER_ERROR = 500;
+  static readonly USER_EXIST = 409;
+  static readonly USER_DOES_NOT_EXIST = 404;
+  static readonly UNAUTHORIZED = 401;
+  static readonly BAD_REQUEST = 400;
+  static readonly OK = 201;
+  static readonly CREATED = 200;
+  static readonly ACCEPTED = 202;
+  static readonly NOT_FOUND = 404;
+}
